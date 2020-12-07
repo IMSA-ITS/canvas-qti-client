e0b5a5d0faa1cbf9ec007901102db4d10d5db3e8
Fred Yankowski <fcy@imsa.edu>
2020-12-07 12:41:11 -0600

    basic debouncing of input text

--- src/Main.elm
+++ src/Main.elm
@@ -3,8 +3,10 @@ module Main exposing (Model)
 -- import Json.Decode as D
 
 import Browser
+import Debounce
 import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (cols, rows)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as D
 import RemoteData exposing (RemoteData(..), WebData, fromResult)
@@ -16,12 +18,15 @@ type alias Flags =
 
 type alias Model =
     { flags : Flags
-    , text : String
+    , rawText : String
+    , settledText : String
+    , debouncer : Debounce.Model String
     }
 
 
 type Msg
     = TextChanged String
+    | DebouncerMsg (Debounce.Msg String)
 
 
 main : Program Flags Model Msg
@@ -36,14 +41,31 @@ main =
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { flags = flags, text = "" }, Cmd.none )
+    ( { flags = flags, rawText = "", settledText = "", debouncer = Debounce.init 500 "" }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case Debug.log "msg" msg of
         TextChanged newText ->
-            ( { model | text = newText }, Cmd.none )
+            { model | rawText = newText } |> updateDebouncer (Debounce.Change newText)
+
+        DebouncerMsg dmsg ->
+            updateDebouncer dmsg model
+
+updateDebouncer : Debounce.Msg String -> Model -> ( Model, Cmd Msg )
+updateDebouncer dmsg model =
+    let
+        ( debouncer_, cmd, settledMaybe ) =
+            Debounce.update dmsg model.debouncer
+
+        debouncedInput_ =
+            settledMaybe |> Maybe.withDefault model.settledText
+    in
+    ( { model | debouncer = debouncer_, settledText = debouncedInput_ }
+    , Cmd.map DebouncerMsg cmd
+    )
+
 
 
 view : Model -> Browser.Document Msg
@@ -52,6 +74,7 @@ view model =
     , body =
         [ div []
             [ div [] [ text (Debug.toString model) ]
+            , Html.textarea [ onInput TextChanged, cols 80, rows 10 ] []
             ]
         ]
     }
